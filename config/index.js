aa7b04ae29d95ecbc24d10ed3a9b4035c8c9cf74
Gandhar Vabale <vabalegandhar@gmail.com>
2023-09-09 12:32:46 +0530

    Basic structure

---
+++ config/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+dotenv.config()
+
+mongoose.connect(
+	process.env.MONGODB_URI
+		? process.env.MONGODB_URI
+		: "mongodb://localhost:27017"
+)
+
+export default {
+	PORT: process.env.PORT,
+	MONGODB_URI: process.env.MONGODB_URI,
+}
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require("express")
+require("dotenv").config()
+import bodyParser from "body-parser"
+import express from "express"
+import config from "./config"
+import apiRouter from "./Routers/apiRouter"
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use("/", apiRouter)
+
+app.listen(config.PORT, () => {
+	console.log("Server started at " + config.PORT)
+})
